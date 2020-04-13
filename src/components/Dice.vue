d03eb07353056ebfcc80f6eea0ed90be67b574fb
THOMAS DAGHELET <thomas.daghelet@adeo.com>
2020-04-13 10:34:01 +0200

    feat(dice): mamayoo dice (2)

--- src/components/Dice.vue
+++ src/components/Dice.vue
@@ -9,23 +9,18 @@
       }"
       :data-roll="roll" id="die-1"
       >
-        <li class="die-item" data-side="1" >
-          <span class="die-face red--text">♦</span>
-        </li>
-        <li class="die-item" data-side="2">
-          <span class="die-face red--text">♥</span>
-        </li>
-        <li class="die-item" data-side="3">
-          <span class="die-face">♣</span>
-        </li>
-        <li class="die-item" data-side="4">
-          <span class="die-face">♠</span>
-        </li>
-        <li class="die-item" data-side="5">
-          <span class="die-face red--text">♦</span>
-        </li>
-        <li class="die-item" data-side="6">
-          <span class="die-face">♣</span>
+        <li
+          class="die-item"
+          v-for="(face, index) in faces"
+          :key="`face-${index}`"
+          :data-side="face.symbol"
+        >
+          <span
+            class="die-face"
+            :class="{'red--text': face.isRedSuit}"
+          >
+            {{ face.value }}
+          </span>
         </li>
       </ol>
     </div>
@@ -53,6 +48,14 @@ export default {
   data() {
     return {
       sides: 6,
+      faces: [
+        { symbol: 'CLUB', value: '♣' },
+        { symbol: 'DIAMOND', value: '♦', isRedSuit: true },
+        { symbol: 'SPADE', value: '♠' },
+        { symbol: 'HEART', value: '♥', isRedSuit: true },
+        { symbol: 'FAKE', value: '♣' },
+        { symbol: 'FAKE2', value: '♦', isRedSuit: true },
+      ],
       dataRoll: 1,
       roll: 1,
       oddRoll: false,
@@ -64,18 +67,7 @@ export default {
     rollDice() {
       this.oddRoll = !this.oddRoll;
       this.evenRoll = !this.evenRoll;
-      this.roll = this.getRandomNumber(1, 4);
-    },
-
-    toggleClasses(die) {
-      die.classList.toggle('odd-roll');
-      die.classList.toggle('even-roll');
-    },
-
-    getRandomNumber(min, max) {
-      const min2 = Math.ceil(min);
-      const max2 = Math.floor(max);
-      return Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
+      this.roll = 'HEART';
     },
   },
 };
@@ -139,58 +131,58 @@ export default {
     justify-self: center;
     font-size: 4em;
   }
-  .even-roll[data-roll="1"] {
+  .even-roll[data-roll="CLUB"] {
     transform: rotateX(360deg) rotateY(720deg) rotateZ(360deg);
   }
-  .even-roll[data-roll="2"] {
+  .even-roll[data-roll="DIAMOND"] {
     transform: rotateX(450deg) rotateY(720deg) rotateZ(360deg);
   }
-  .even-roll[data-roll="3"] {
+  .even-roll[data-roll="SPADE"] {
     transform: rotateX(360deg) rotateY(630deg) rotateZ(360deg);
   }
-  .even-roll[data-roll="4"] {
+  .even-roll[data-roll="HEART"] {
     transform: rotateX(360deg) rotateY(810deg) rotateZ(360deg);
   }
-  .even-roll[data-roll="5"] {
+  .even-roll[data-roll="FAKE"] {
     transform: rotateX(270deg) rotateY(720deg) rotateZ(360deg);
   }
-  .even-roll[data-roll="6"] {
+  .even-roll[data-roll="FAKE2"] {
     transform: rotateX(360deg) rotateY(900deg) rotateZ(360deg);
   }
-  .odd-roll[data-roll="1"] {
+  .odd-roll[data-roll="CLUB"] {
     transform: rotateX(-360deg) rotateY(-720deg) rotateZ(-360deg);
   }
-  .odd-roll[data-roll="2"] {
+  .odd-roll[data-roll="DIAMOND"] {
     transform: rotateX(-270deg) rotateY(-720deg) rotateZ(-360deg);
   }
-  .odd-roll[data-roll="3"] {
+  .odd-roll[data-roll="SPADE"] {
     transform: rotateX(-360deg) rotateY(-810deg) rotateZ(-360deg);
   }
-  .odd-roll[data-roll="4"] {
+  .odd-roll[data-roll="HEART"] {
     transform: rotateX(-360deg) rotateY(-630deg) rotateZ(-360deg);
   }
-  .odd-roll[data-roll="5"] {
+  .odd-roll[data-roll="FAKE"] {
     transform: rotateX(-450deg) rotateY(-720deg) rotateZ(-360deg);
   }
-  .odd-roll[data-roll="6"] {
+  .odd-roll[data-roll="FAKE2"] {
     transform: rotateX(-360deg) rotateY(-900deg) rotateZ(-360deg);
   }
-  [data-side="1"] {
+  [data-side="CLUB"] {
     transform: rotate3d(0, 0, 0, 90deg) translateZ(3rem);
   }
-  [data-side="2"] {
+  [data-side="DIAMOND"] {
     transform: rotate3d(-1, 0, 0, 90deg) translateZ(3rem);
   }
-  [data-side="3"] {
+  [data-side="SPADE"] {
     transform: rotate3d(0, 1, 0, 90deg) translateZ(3rem);
   }
-  [data-side="4"] {
+  [data-side="HEART"] {
     transform: rotate3d(0, -1, 0, 90deg) translateZ(3rem);
   }
-  [data-side="5"] {
+  [data-side="FAKE"] {
     transform: rotate3d(1, 0, 0, 90deg) translateZ(3rem);
   }
-  [data-side="6"] {
+  [data-side="FAKE2"] {
     transform: rotate3d(1, 0, 0, 180deg) translateZ(3rem);
   }
 
