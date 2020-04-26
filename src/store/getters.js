530c7c025c56183ea2b72295144051e4c0dfc681
Florian VANDENBULCKE <florian.vandenbulcke@ext.adeo.com>
2020-04-26 15:44:29 +0200

    feat(store): getters for store reorganization

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,44 +3,55 @@ import groupBy from 'lodash/groupBy';
 const sortCards = (cardOne, cardTwo) => cardOne.value - cardTwo.value;
 
 export default {
+  playerName({ playerName }) {
+    return playerName;
+  },
+
   isConnected({ isConnected }) {
     return isConnected;
   },
-  playerStatus({ player }) {
-    const {
-      name, score, state, playedCard,
-    } = player;
-    return {
-      name,
-      score,
-      isTurn: state === 'IS_PLAYING',
-      playedCard,
-    };
+
+  players({ players }) {
+    return players && players.map((p) => {
+      const player = {
+        ...p,
+        isTurn: p.state === 'IS_PLAYING',
+      };
+      delete player.state;
+      return player;
+    });
   },
 
-  playerCards({ orderedSuits, player }) {
+  groupedPlayers({ playerName }, { players }) {
+    const groupPlayers = players && groupBy(players, (p) => p.name === playerName);
+    return groupPlayers;
+  },
+
+  localPlayer(state, { groupedPlayers }) {
+    return groupedPlayers && groupedPlayers.true[0];
+  },
+
+  otherPlayers(state, { groupedPlayers, localPlayer }) {
+    const otherPlayers = groupedPlayers && groupedPlayers.false;
+    if (otherPlayers && otherPlayers.length > 1
+      && otherPlayers.some((p) => p.order < localPlayer.order)) {
+      while (otherPlayers.slice(-1).pop().order > localPlayer.order) {
+        otherPlayers.unshift(otherPlayers.pop());
+      }
+    }
+    return otherPlayers;
+  },
+
+  playerCards({ orderedSuits, playerCards }) {
+    if (!playerCards) { return []; }
     const sortedCards = [];
-    const groupedCards = groupBy(player.cards, 'suit');
+    const groupedCards = groupBy(playerCards, 'suit');
     orderedSuits.forEach((suit) => {
       if (groupedCards[suit]) {
         sortedCards.push(...groupedCards[suit].sort(sortCards));
       }
     });
     return sortedCards;
-  },
-
-  otherPlayers({ otherPlayers }) {
-    return otherPlayers.map((p) => {
-      const {
-        name, score, state, playedCard,
-      } = p;
-      return {
-        name,
-        score,
-        isTurn: state === 'IS_PLAYING',
-        playedCard,
-      };
-    });
   },
 
   gameState({ gameState }) {
